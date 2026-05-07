f467ffab396c0239666d980a37eaf2af4b622e61
ibrahim-badshah <ibrahimbadshah.mb@gmail.com>
2026-05-07 10:08:24 +0530

    chunk size issue fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('framer-motion')) return 'vendor-framer';
+            if (id.includes('recharts')) return 'vendor-charts';
+            if (id.includes('lucide-react')) return 'vendor-icons';
+            if (id.includes('highlight.js')) return 'vendor-highlight';
+            if (id.includes('@supabase')) return 'vendor-supabase';
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) return 'vendor-react';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
